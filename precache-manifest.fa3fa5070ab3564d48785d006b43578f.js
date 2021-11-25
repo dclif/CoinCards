ec8a74f871fb2d1a3d63c65cb44bd5185d973980
Daniel <65896341+dclif@users.noreply.github.com>
2021-11-25 13:10:31 +0000

    Updates

---
+++ precache-manifest.fa3fa5070ab3564d48785d006b43578f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5b018054c55104122e5deacccc07f6",
+    "url": "/CoinCards/index.html"
+  },
+  {
+    "revision": "7734912e7b2e74dc04ef",
+    "url": "/CoinCards/static/css/main.fb290410.chunk.css"
+  },
+  {
+    "revision": "d2e6836a6c4c536c991b",
+    "url": "/CoinCards/static/js/2.d4d6b12a.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/CoinCards/static/js/2.d4d6b12a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eae20cf289ce81cd90a1",
+    "url": "/CoinCards/static/js/3.b0456ff3.chunk.js"
+  },
+  {
+    "revision": "7734912e7b2e74dc04ef",
+    "url": "/CoinCards/static/js/main.1f5f50ed.chunk.js"
+  },
+  {
+    "revision": "21d71a3020183d65e991",
+    "url": "/CoinCards/static/js/runtime-main.05356e57.js"
+  }
+]);
